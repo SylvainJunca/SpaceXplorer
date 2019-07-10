43a5b0872b167ccb86227e1ee72dfa7c2fcb92cb
SylvainJunca <sylvain.junca@gmail.com>
2019-07-10 12:28:49 -0400

    connects spacex API

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -5,6 +5,54 @@ class LaunchAPI extends RESTDataSource {
     super();
     this.baseURL = 'https://api.spacexdata.com/v2/';
   }
+  async getAllLaunches() {
+    //this get makes a get request to https://api.spacexdata.com/v2/launches
+    const response = await this.get('launches');
+    return Array.isArray(response)
+      ? response.map(launch => this.launchReducer(launch))
+      : [];
+  }
+  async getLaunchById({ launchId }) {
+    const response = await this.get('launches', { flight_number: launchId });
+    return this.launchReducer(response[0]);
+  }
+  
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId })),
+    );
+  }
+
+  /* 
+
+  we use a reducer in order to keep the getAllLaunches as consice and clear as possible
+  this gets the data we receive from the API and reduces it to the format our schema expects
+  
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+  */
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
 }
 
 module.exports = LaunchAPI;
